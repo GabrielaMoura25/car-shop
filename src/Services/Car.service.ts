95b747f7874a63cfa5cd45ed6281b5c09c51dc5f
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-14 15:04:44 -0300

    criando car service

---
+++ src/Services/Car.service.ts
@@ -0,0 +1,18 @@
+import Car from '../Domains/Car';
+import Icar from '../Interfaces/Icar';
+import CarModel from '../Models/Car.model';
+
+export default class CarService {
+  private _carModel = new CarModel();
+
+  private createCarDomain(car: Icar) {
+    if (car) return new Car(car);
+    return null;
+  }
+
+  public async create(car: Icar) {
+    const carCreated = await this._carModel.create(car);
+
+    return this.createCarDomain(carCreated);
+  }
+}
