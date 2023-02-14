fdfa6ca7a39693ae17e6b9937851c770fe2b0713
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-14 14:57:59 -0300

    criando a interface Icar

--- src/Models/Car.model.ts
+++ src/Models/Car.model.ts
@@ -7,7 +7,7 @@ export default class CarModel {
 
   constructor() {
     this.schema = new Schema<Icar>({
-      _model: { type: String, required: true },
+      model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
       status: { type: Boolean, required: true },
