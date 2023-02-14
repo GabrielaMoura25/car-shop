55dd170d1bb8c970ff0d47ef7b62776298be60a8
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-14 15:17:39 -0300

    criando car controller

---
+++ src/Controllers/Car.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import Icar from '../Interfaces/Icar';
+import CarService from '../Services/Car.service';
+
+export default class CarController {
+  private _req: Request;
+  private _res: Response;
+  private _next: NextFunction;
+  private _carService: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this._req = req;
+    this._res = res;
+    this._next = next;
+    this._carService = new CarService();
+  }
+
+  public async create() {
+    const car: Icar = { ...this._req.body };
+
+    try {
+      const carCreated = await this._carService.create(car);
+
+      return this._res.status(201).json(carCreated);
+    } catch (error) {
+      return this._res.status(500).json(error);
+    }
+  }
+}
