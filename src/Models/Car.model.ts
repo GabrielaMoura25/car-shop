fd229689f79ebe0f592b83944d6977f91f1f7fa3
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 18:24:27 -0300

    refatorando car model

--- src/Models/Car.model.ts
+++ src/Models/Car.model.ts
@@ -1,5 +1,4 @@
 import { Schema } from 'mongoose';
-import { ObjectId } from 'mongodb';
 import ICar from '../Interfaces/ICar';
 import AbstractODM from './AbstractODM';
 
@@ -14,21 +13,5 @@ export default class CarModel extends AbstractODM<ICar> {
       doorsQty: { type: Number, required: true },
       seatsQty: { type: Number, required: true },
     }));
-  }
-
-  public async create(car: ICar): Promise<ICar> {
-    return this.model.create({ ...car });
-  }
-
-  public async findAll(): Promise<ICar[]> {
-    return this.model.find();
-  }
-
-  public async findById(id: string): Promise<ICar | null> {
-    return this.model.findOne({ _id: new ObjectId(id) });
-  }
-
-  public async updateById(id: string, car: ICar): Promise<ICar | null> {
-    return this.model.findOneAndUpdate({ _id: new ObjectId(id) }, car, { new: true });
   }
 }
