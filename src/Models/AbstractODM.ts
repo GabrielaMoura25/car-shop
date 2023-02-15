af0366304d22009c4343f95d3193e14ac363b87a
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 17:29:31 -0300

    refatora abstract model

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -5,7 +5,7 @@ export default abstract class AbstractODM <T> {
   protected model: Model<T>;
   protected name: string;
 
-  constructor(schema: Schema, name: string) {
+  constructor(name: string, schema: Schema) {
     this.name = name;
     this.schema = schema;
     this.model = models[this.name] || model(this.name, this.schema);
