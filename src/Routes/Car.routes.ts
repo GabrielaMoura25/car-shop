bd7d967b496768cc0d7dbc8d7d2736576c0372aa
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 17:25:46 -0300

    refatora car routes

--- src/Routes/Car.routes.ts
+++ src/Routes/Car.routes.ts
@@ -3,25 +3,9 @@ import CarController from '../Controllers/Car.controller';
 
 const router = Router();
 
-router.put('/:id', (req, res, next) => {
-  const carController = new CarController(req, res, next);
-
-  return carController.updateById();
-});
-router.get('/:id', (req, res, next) => {
-  const carController = new CarController(req, res, next);
-
-  return carController.findById();
-});
-router.get('/', (req, res, next) => {
-  const carController = new CarController(req, res, next);
-
-  return carController.findAll();
-});
-router.post('/', (req, res, next) => {
-  const carController = new CarController(req, res, next);
-
-  return carController.create();
-});
+router.put('/:id', (req, res, next) => new CarController(req, res, next).updateById());
+router.get('/:id', (req, res, next) => new CarController(req, res, next).findById());
+router.get('/', (req, res, next) => new CarController(req, res, next).findAll());
+router.post('/', (req, res, next) => new CarController(req, res, next).create());
 
 export default router;
