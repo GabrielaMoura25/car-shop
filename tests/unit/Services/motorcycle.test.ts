d76e61513853bf3fbe37ffe5c50256158c178596
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 18:08:44 -0300

    :sparkles: implementa os testes de motorcycle service e finaliza req 09

--- tests/unit/Services/motorcycle.test.ts
+++ tests/unit/Services/motorcycle.test.ts
@@ -31,4 +31,35 @@ describe('Motorcycle Service', function () {
     expect(motorcycle).to.have.property('status');
     expect(motorcycle).to.be.deep.equal({ id, ...reqMotorcycle });
   });
+
+  it('should find all motorcycles', async function () {
+    sinon.stub(Model, 'find').resolves([{ id, ...reqMotorcycle }]);
+
+    const motorcycleService = new MotorcycleService();
+    const motorcycle = await motorcycleService.findAll();
+
+    expect(motorcycle).to.be.deep.equal([{ id, ...reqMotorcycle }]);
+  });
+
+  it('should find a motorcycle by id', async function () {
+    sinon.stub(Model, 'findOne').resolves({ id, ...reqMotorcycle });
+
+    const motorcycleService = new MotorcycleService();
+    const motorcycle = await motorcycleService.findById(id);
+
+    const response = { id, ...reqMotorcycle };
+
+    expect(motorcycle).to.be.deep.equal({ status: 200, response });
+  });
+
+  it('should find a motorcycle by id and update', async function () {
+    sinon.stub(Model, 'findOneAndUpdate').resolves({ id, ...reqMotorcycle });
+
+    const motorcycleService = new MotorcycleService();
+    const motorcycle = await motorcycleService.updateById(id, reqMotorcycle);
+
+    const response = { id, ...reqMotorcycle };
+
+    expect(motorcycle).to.be.deep.equal({ status: 200, response });
+  });
 });
