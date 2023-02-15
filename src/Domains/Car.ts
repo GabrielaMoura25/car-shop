8b160ccafdcfa9a05afe9289ceba2b07ad763367
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 16:31:22 -0300

    refatora car domain

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,22 +1,12 @@
 import Icar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id?: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean | undefined;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(car: Icar) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status;
-    this.buyValue = car.buyValue;
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
   }
