e0dd8ba73691271e95d38e6f4dd3c0277ebbfb81
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 17:31:11 -0300

    :sparkles: finaliza req 05

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import carRoutes from './Routes/Car.routes';
+import motorcycleRoutes from './Routes/Motorcycle.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRoutes);
+app.use('/motorcycles', motorcycleRoutes);
 
 export default app;
