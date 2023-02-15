6f16ae014898eb9b100b87ae38f8977c7842753c
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-15 18:24:04 -0300

    refatorando o AbstractODM par incluir as funções de create, findAll, findById e updateById

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,4 +1,5 @@
-import { Model, Schema, model, models } from 'mongoose';
+import { Model, Schema, model, models, UpdateQuery } from 'mongoose';
+import { ObjectId } from 'mongodb';
 
 export default abstract class AbstractODM <T> {
   protected schema: Schema;
@@ -9,5 +10,21 @@ export default abstract class AbstractODM <T> {
     this.name = name;
     this.schema = schema;
     this.model = models[this.name] || model(this.name, this.schema);
+  }
+
+  public async create(car: T): Promise<T> {
+    return this.model.create({ ...car });
+  }
+
+  public async findAll(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  public async findById(id: string): Promise<T | null> {
+    return this.model.findOne({ _id: new ObjectId(id) });
+  }
+
+  public async updateById(id: string, vehicle: UpdateQuery<T>): Promise<T | null> {
+    return this.model.findOneAndUpdate({ _id: new ObjectId(id) }, { ...vehicle }, { new: true });
   }
 }
